03e8ff5b247d69d6243814eda7990122d682c05a
Goran <goran.brkuljan@gmail.com>
2023-01-19 22:08:31 +0100

    app: prepare for app dev

---
+++ src/features/app/components/LazyAppLoad.jsx
@@ -0,0 +1,53 @@
+import React, { useEffect } from 'react';
+import Box from '@mui/material/Box';
+import { useSelector } from 'react-redux';
+import { Navigate, Route, Routes } from 'react-router-dom';
+
+/* nodes */
+import NodesIndex from '../../../pages/nodes/Index';
+import NodeShow from '../../../pages/nodes/Show';
+
+/* users */
+import UserAuthentication from '../../../pages/users/Authentication';
+import useUserAuthentication from '../../authentication/hooks/useUserAuthentication';
+/* nodecosmos */
+import { HEADER_HEIGHT } from '../constants';
+import Header from './header/Header';
+
+export default function LazyAppLoad() {
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+  const currentUser = useSelector((state) => state.auth.currentUser);
+
+  const { syncUpCurrentUser } = useUserAuthentication();
+
+  useEffect(() => {
+    if (isAuthenticated) syncUpCurrentUser();
+  }, [isAuthenticated, syncUpCurrentUser]);
+
+  return (
+    <Box
+      borderRadius={{
+        xs: 0,
+        sm: 2,
+      }}
+      height={1}
+      overflow="auto"
+      border={1}
+      borderColor="borders.2"
+    >
+      <Header />
+      <Box height={1} pt={`${HEADER_HEIGHT}px`}>
+        <Routes>
+          <Route path="/n" element={(<NodesIndex />)} />
+          <Route
+            path="/login"
+            element={
+              isAuthenticated ? <Navigate to={`/users/${currentUser.username}`} /> : <UserAuthentication />
+            }
+          />
+          <Route path="/nodes/:id/*" element={<NodeShow />} />
+        </Routes>
+      </Box>
+    </Box>
+  );
+}
