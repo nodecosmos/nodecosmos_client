21429bd5a6f94dbecf3c995d1da000bdabe11039
Goran <goran.brkuljan@gmail.com>
2023-02-10 22:54:00 +0100

    fix nested nodes branch

--- src/features/nodes/components/tree/NestedNodesBranch.jsx
+++ src/features/nodes/components/tree/NestedNodesBranch.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useTheme } from '@mui/material';
 import * as PropTypes from 'prop-types';
 import { useSelector } from 'react-redux';
+import usePrevProps from '../../../../common/hooks/usePrevProps';
 import { selectIsNodeExpandedById, selectNodePositionById } from '../../nodes.selectors';
 
 import {
@@ -18,20 +19,23 @@ export default function NestedNodesBranch(props) {
   const isExpanded = useSelector(selectIsNodeExpandedById(id));
   const { xEnds, y } = useSelector(selectNodePositionById(id));
   const { y: pathYEnds } = useSelector(selectNodePositionById(lastChildId));
+  const prevPathYEnds = usePrevProps(pathYEnds);
 
   const theme = useTheme();
 
   const x = xEnds + MARGIN_LEFT;
   const linkY = y + MARGIN_TOP;
 
-  if (!isExpanded || !lastChildId) return null;
+  const yEnds = pathYEnds || prevPathYEnds;
+
+  if (!isExpanded || !lastChildId || !yEnds) return null;
 
   return (
     <path
       stroke={theme.palette.tree.default}
       fill="transparent"
       strokeWidth={3.5}
-      d={`M ${x} ${linkY} L ${x} ${pathYEnds}`}
+      d={`M ${x} ${linkY} L ${x} ${yEnds}`}
       style={{
         opacity: 0,
         animation: `node-path-appear ${INITIAL_ANIMATION_DURATION}ms ${INITIAL_ANIMATION_DELAY}ms forwards`,
