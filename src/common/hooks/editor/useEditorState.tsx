83574087805ea4ebfdfb5c911931857b5bf7b85e
Goran Brkuljan <goran.brkuljan@gmail.com>
2024-12-27 21:22:59 +0100

    udate

--- src/common/hooks/editor/useEditorState.tsx
+++ src/common/hooks/editor/useEditorState.tsx
@@ -1,21 +1,26 @@
 import { useEditorContext } from './useEditorContext';
 import { EditorState } from 'prosemirror-state';
-import { useEffect, useState } from 'react';
+import {
+    useCallback, useEffect, useState,
+} from 'react';
 
 export default function useEditorState(): EditorState | null {
     const { editorView } = useEditorContext();
     const [state, setState] = useState(editorView?.state);
 
+    const reset = useCallback(() => {
+        setState(editorView!.state);
+    }, [editorView]);
+
     useEffect(() => {
         if (!editorView) return;
 
-        const reset = () => setState(editorView.state);
         editorView.dom.addEventListener('pm-state-change', reset);
 
         return () => {
             editorView?.dom?.removeEventListener('pm-state-change', reset);
         };
-    }, [editorView]);
+    }, [editorView, reset]);
 
     return state || null;
 }
